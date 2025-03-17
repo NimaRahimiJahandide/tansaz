1332e9f995663487485eb2b7ec71a91f436c8de7
Nima <nimarahimijahandide@gmail.com>
2025-03-17 12:53:45 +0330

    bug fix blogs page

--- components/blog/CardComponent.vue
+++ components/blog/CardComponent.vue
@@ -9,12 +9,12 @@ defineProps({
 <template>
     <NuxtLink
     :to="route"
-    class="rounded-3xl shadow-lg p-4 group cursor-pointer"
+    class="rounded-3xl group cursor-pointer"
   >
     <div class="overflow-hidden rounded-2xl">
       <img 
         :src="image" 
-        class="rounded-2xl mb-4 w-full transform transition-transform duration-300 group-hover:scale-110" 
+        class="rounded-3xl shadow-lg p-4 mb-4 w-full transform transition-transform overflow-hidden duration-300 group-hover:scale-110" 
         :alt="title" 
       />
     </div>
