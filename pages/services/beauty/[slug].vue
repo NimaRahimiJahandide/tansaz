eb64a612105e945d0d830b9033c745e2604d1d0e
mahdiMd <mahdi.daee.ch@gmail.com>
2025-06-16 09:07:56 -0700

    some changes

---
+++ pages/services/beauty/[slug].vue
@@ -0,0 +1,95 @@
+<script setup>
+import { useStartWebsite } from "@/store/initWebsite";
+const startWebsite = useStartWebsite();
+
+onMounted(() => {
+  startWebsite.setImageClicked(true);
+});
+</script>
+
+<template>
+  <div>
+    <div class="w-full h-[354px] relative">
+      <div
+        class="absolute top-0 left-0 w-full h-[354px] bg-[#000000af] z-[2]"
+      />
+      <img src="/images/blog1.png" alt="" class="w-full h-[354px]" />
+      <div class="flex flex-col absolute top-[94px] right-[16px] z-[3]">
+        <p class="text-[#ED1C24] text-[30px] font-bold">تزریقات</p>
+        <p class="text-[#FFFFFF] text-[30px] font-bold">زیبایی</p>
+      </div>
+
+      <div
+        class="grid grid-cols-3 gap-[10px] px-[16px] w-full absolute top-[198px] right-0 z-[3]"
+      >
+        <div
+          v-for="x in 6"
+          class="h-[38px] rounded-[30px] flex items-center justify-center backdrop-blur-xl inset-shadow-[0px_-5px_21px_0px_#e3edef41,0px_70px_47px_-74px_#CAACFF4D]"
+        >
+          <p class="text-[14px] text-[#ffffff]">بوتاکس</p>
+        </div>
+      </div>
+    </div>
+    <div
+      class="flex flex-col pt-[20px] px-[16px] mt-[-40px] relative bg-[#F8F8F8] rounded-t-[40px] pb-[45px] z-[10]"
+    >
+      <p class="text-[20px] font-bold">
+        <span class="text-[#ED1C24]">مزوتراپی</span>
+        <span class="text-[#000000]"> چیست ؟</span>
+      </p>
+
+      <p
+        class="text-[16px] font-semibold text-[#2E2E2E] leading-[30px] mt-[10px]"
+      >
+        مزوتراپی تکنیکی است برای جوانسازی ، سفت کردن و بازسازی پوست که توسط
+        تزریق عصاره گیاهان ، ویتامینها ، هورمونها در پوست انجام می گیرید . این
+        تکنیک همچنین برای حذف چربی ها نیز کاربرد دارد .مزوتراپی باعث تغذیه و
+        جوان سازی پوست توسط تولید کلاژن و الاستین می شود و متابولیسم پوست شما را
+        تحریک می کند.این تکنیک در سال ۱۹۵۲ توسط پزشک فرانسوی به نام دکتر میکاییل
+        پیستور توسعه یافت . در آن زمان از مزوتزاپی بیشتر برای کاهش درد استفاده
+        می شد. ولی در سال های بعد محبوبیت زیادی در ایالات آمریکا و دیگر نقاط
+        جهان پیدا کرد . کاربرد های مزوتراپی: پیشگیری از چین و چروک و جوانسازی
+        پوست، محو کردن خطوط پوست، بهبود ، روشنایی پوست ملتهب و قرمز، سفت کردن
+        پوست شل می باشد.
+      </p>
+    </div>
+
+    <HomeBeforAfterExample />
+    <ServicesVideoSlider :data="['/images/blog1.png']" />
+    <HomeFaqSection />
+
+    <div
+      class="flex flex-col w-full mt-[30px] bg-[#F5F5F5] px-[16px] py-[30px] relative"
+    >
+
+    <div class="absolute bottom-0 right-0 w-full bg-gradient-to-t h-[200px] from-[#00000016] to-[#00000000]">
+
+    </div>
+
+      <p class="text-[20px] font-bold">
+        <span class="text-[#ED1C24]"> متریال درجه یک </span>
+
+        <span class="text-[#000000]"> برای مزوتراپی </span>
+
+        <span class="text-[#ED1C24]"> ! </span>
+      </p>
+      <p class="text-[20px] font-bold mt-[40px]">
+        <span class="text-[#000000]"> مزو تراپی در کمتر از 20 دقیقه، </span>
+
+        <span class="text-[#ED1C24]"> بدون درد </span>
+
+        <span class="text-[#000000]"> و با </span>
+        <span class="text-[#ED1C24]"> بی حسی </span>
+        <span class="text-[#000000]"> موضعی انجام می شود. </span>
+      </p>
+    </div>
+
+    <Comments />
+  </div>
+</template>
+
+<style scoped>
+/* .custom-shadow {
+  box-shadow: inset;
+} */
+</style>
