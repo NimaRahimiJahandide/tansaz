8a646c80756dfd6ed75b636ee84a59df87fdb324
Nima <nimarahimijahandide@gmail.com>
2025-07-21 00:58:23 +0330

    about us is done

---
+++ components/aboutUs/EventsSection.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="bg-white">
+    <img class="" src="/images/blog-banner.png" alt="blog-banner" />
+    <h2 class="text-xl font-bold leading-7 text-center pb-4 pt-[30px]">
+      <span class="text-brand">رویداد های</span> کلینیک تن ساز
+    </h2>
+    <div class="container mx-auto px-4 pb-8">
+      <div class="rounded-2xl overflow-hidden relative">
+        <transition name="fade" mode="out-in">
+          <div :key="activeIndex" class="relative" @touchstart="handleTouchStart" @touchend="handleTouchEnd">
+            <!-- Image -->
+            <img :src="slides[activeIndex].image" :alt="slides[activeIndex].title" class="w-full h-80 object-cover" />
+
+            <!-- Overlay and Text -->
+            <div
+              class="absolute inset-0 bg-gradient-to-t from-[#151515] via-[#15151596] to-transparent via-[20.35%] from-[1.3%] flex flex-col justify-end p-4">
+              <h3 class="text-lg font-bold text-white leading-6 mb-5">
+                {{ slides[activeIndex].title }}
+              </h3>
+              <p class="flex text-xs font-medium text-white justify-between">
+                <span>تاریخ برگزاری: {{ slides[activeIndex].date }}</span>
+              </p>
+            </div>
+          </div>
+        </transition>
+      </div>
+
+      <!-- pagination -->
+      <div class="flex justify-between items-center mt-4">
+        <!-- دکمه قبلی -->
+        <button @click="prevSlide"
+          class="bg-brand size-10 flex items-center justify-center text-white font-bold rounded-lg">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+          </svg>
+        </button>
+
+        <!-- شماره‌های صفحه -->
+        <div class="flex items-center gap-2">
+          <div v-for="(slide, index) in slides.length" :key="index" @click="activeIndex = index"
+            class="w-8 h-8 rounded-xl cursor-pointer transition duration-300 ease-in-out flex items-center justify-center text-sm font-medium"
+            :class="{ 'bg-[#0000000D] text-[#2E2E2E]': activeIndex === index, 'text-[#929DAC]': activeIndex !== index }">
+            {{ index + 1 }}
+          </div>
+        </div>
+
+        <!-- دکمه بعدی -->
+        <button @click="nextSlide"
+          class="bg-[#D4D4D4] text-[#929DAC] font-bold size-10 flex items-center justify-center rounded-lg">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 rotate-180" fill="none" viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  isTransparent: Boolean,
+});
+
+const route = useRoute();
+
+const activeIndex = ref(0);
+
+const slides = ref([
+  {
+    image: "/images/seminar.jpg",
+    title: "برگزاری سمینار آبان 1405",
+    duration: "2 دقیقه",
+    date: "1404/01/04",
+  },
+  {
+    image: "/images/blog1.png",
+    title: "تغذیه مناسب برای رشد مو",
+    duration: "3 دقیقه",
+    date: "1404/01/10",
+  },
+  {
+    image: "/images/blog1.png",
+    title: "راهکارهای جلوگیری از ریزش مو",
+    duration: "4 دقیقه",
+    date: "1404/01/18",
+  },
+]);
+
+const nextSlide = () => {
+  activeIndex.value = (activeIndex.value + 1) % slides.value.length;
+};
+
+const prevSlide = () => {
+  activeIndex.value =
+    (activeIndex.value - 1 + slides.value.length) % slides.value.length;
+};
+
+const touchStartX = ref(0);
+const touchEndX = ref(0);
+
+const handleTouchStart = (e) => {
+  touchStartX.value = e.changedTouches[0].screenX;
+};
+
+const handleTouchEnd = (e) => {
+  touchEndX.value = e.changedTouches[0].screenX;
+  handleSwipe();
+};
+
+const handleSwipe = () => {
+  const diff = touchStartX.value - touchEndX.value;
+  if (Math.abs(diff) > 50) {
+    if (diff > 0) {
+      // کشیدن به چپ → بعدی
+      prevSlide();
+    } else {
+      // کشیدن به راست ← قبلی
+      nextSlide();
+    }
+  }
+};
+
+</script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- pages/about-us/index.vue
+++ pages/about-us/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { AboutUsEventsSection } from "#components";
 import { useStartWebsite } from "@/store/initWebsite";
 const startWebsite = useStartWebsite();
 const router = useRouter();
@@ -12,22 +13,99 @@ const goBack = () => {
   }
 };
 const gallery = [
-  "/images/Surgery1.png",
-
-  "/images/Surgery2.png",
-
-  "/images/Surgery1.png",
-
-  "/images/Surgery1.png",
+  "/images/clinic3.jpg",
+  "/images/clinic1.jpg",
+  "/images/clinic1.jpg",
+  "/images/clinic1.jpg",
+  "/images/clinic1.jpg",
 ];
+
+const whyTansaz = [
+  {
+    id: 0,
+    title: "توجه و احترام به مراجعین",
+    description: 'احترام به مراجعین و نیازهای آنها و توجه به کیفیت ارئه خدمات در بالاترین سطح استانداردهای بین المللی مهمترین اصل در همه فرایندهای جاری مجموعه تن ساز است .'
+  },
+  {
+    id: 1,
+    title: "آنالیز اختصاصی",
+    description: 'قبل از هر اقدام تخصصی و درمانی آنالیز کامل با پیشرفته ترین نکنولوژی روزدنیا و معاینه دقیق توسط پزشکان مجموعه انجام می گردد تا بهترین پیشنهاد اختصاصی برای هر فرد متناسب با اویتهای و هزینههای درمانی و با بیشترین اثر بخشی و ماندگاری توصیه شود تا تجربه ای متفاوت از مشاوره تخصصی دلسوزانه و مسئولانه ارائه گردد .'
+  },
+  {
+    id: 2,
+    title: "پزشکان متخصص",
+    description: 'پزشکان متخصص و فوق تخصص و با تجربه و عضو انجمن های پزشکی ایران و سایر کشورهای اروپایی و آمریکا دارای مدارک معتبر بین المللی دورههای پیشرفته تخصصی پزشکی استتیک با جدیدترین متدها و آشنا به پیشرفته ترین تکنولوژی های روز دنیا در صنعت پزشکی زیبایی و لیزر پزشکی و تناسب اندام .'
+  },
+  {
+    id: 3,
+    title: "دستگاه های مجهز",
+    description: 'اولین و تنها مجموعه کامل مجهز به مرکز لیزر اسکلپیون آلمان در جنوب کشور'
+  },
+  {
+    id: 4,
+    title: "چربی سوضی موضعی استاندارد",
+    description: 'مركز لاغری و تناسب اندام مجهز به تنها کویتیشن چربی سوز موضعی مورد تایید امریکا در دنیا شاک ویوتراپی سوئیسی گلد استاندارد درمانی سلولیت به تایید FDA با توان 7 تسلا ICM SLIM مریکا و شبیه ساز ورزشی FDA هر نیم ساعت معادل 20 هزار حرکت ورزشی .'
+  },
+  {
+    id: 5,
+    title: "محیط لوکس",
+    description: 'محیطی لوکس حرفه ای و کاملا بهداشتی با توجه ویژه به حریم خصوصی و آرامش مراجعین محترم مجموعه تن ساز در تمام زمینه ها با استانداردهای بین المللی و تجربه ای متفاوت و خاص .'
+  },
+  {
+    id: 6,
+    title: "تخفیف و جایزه های متنوع",
+    description: 'فراهم آوردن شرایط برخورداری مراجعین محترم از تخفیفات ویژه سیستم cash back و كيف پول باشگاه مشتریان با امکانات و امتیازات بسیار ویژه امکان پرداخت اقساطی هزینه های درمان و پروموشن های متنوع.'
+  },
+]
+
+const doctors = [
+  {
+    id: 0,
+    name: "دکتر شهلا فلاحی",
+    specialty: "دکتری حرفه ای پزشکی (پوست، مو، زیبایی)",
+    description: "بیش از 30 سال تجربه طبابت و تدریس و پژوهش در حوزه پزشکی زیبایی و مراقبت از پوست عضو انجمن علمی استتیک کانادا",
+    image: "/images/doctor1.jpg",
+    education: "فارغ التحصیل پزشکی شیراز",
+    post: [
+      "عضو انجمن علمی آموزشی پزشکی ایران",
+    ],
+    body: "دارای گواهینامه های رسمی دوره های تخصصی لیزر پزشکی و دیگر تکنولوژی های پزشکی زیبایی و تزریقات ژل و فیلر و بوتاکس از سازمان نظام پزشکی کشور عضو انجمن علمی پزشکی لیزر ایران",
+  },
+  {
+    id: 1,
+    name: "دکتر سروش باقری",
+    specialty: "دکترای حرفه ای پزشکی ( پوست ، مو ، جراحی )",
+    description: "بیش از 8 سال سابقه فعالیت در حوزه پزشکی زیبایی",
+    image: "/images/doctor2.jpg",
+    education: "فارغ التحصیل پزشکی شیراز",
+    post: [
+      "عضو انجمن علمی پزشکی لیزر ایران", 
+      "عضو انجمن علمی آموزش پزشکی ایران", 
+      "عضو انجمن علمی تغذیه ایران", 
+    ],
+    body: "دارای گواهینامههای متعدد پزشکی زیبایی معتبر بین المللی از کشورهای اروپایی و مذاکز معتبر علمی کشور",
+  },
+  {
+    id: 2,
+    name: "دکتر سبحانی",
+    specialty: "دکترای حرفه ای پزشکی متخصص بیماریهای داخلی و فوق تخصص بیماری های روماتولوژی)",
+    description: "بیش از 6 سال تجربه طبابت تخصصی و فوق تخصصی",
+    image: "/images/doctor3.jpg",
+    education: "فارغ التحصیل پزشکی شیراز",
+    post: [
+      "عضو انجمن روماتولوژی ایران", 
+      "دارای بورد تخصصی", 
+    ],
+  },
+]
 
 onMounted(() => {
   startWebsite.setImageClicked(true);
 });
 </script>
 <template>
-  <div class="flex flex-col items-center bg-[#EFEFEF] pt-[84px] pb-[50px]">
-    <div class="flex items-center justify-between w-full px-[16px]">
+  <div class="flex flex-col items-center bg-[#EFEFEF] pt-[84px]">
+    <section class="flex items-center justify-between w-full px-[16px]">
       <header class="">
         <h1 class="text-xl font-bold">
           <span class="text-brand">ما</span> درباره
@@ -35,38 +113,33 @@ onMounted(() => {
       </header>
 
       <img @click="goBack" src="/icons/left-arrow.svg" alt="" />
-    </div>
+    </section>
 
-    <div
-      class="relative w-full aspect-square rounded-[16px] overflow-hidden mt-[20px] px-[16px]"
-    >
-      <img
-        v-for="(x, i) in gallery"
-        :src="x"
-        alt=""
-        class="duration-300 absolute w-full aspect-square"
-        :class="i == selectedImage ? 'opacity-100' : 'opacity-0'"
-      />
-    </div>
-    <div
-      class="flex items-center gap-[16px] max-w-full overflow-x-auto justify-center px-[16px]"
-    >
-      <img
-        v-for="(x, i) in gallery"
-        :src="x"
-        alt=""
-        class="size-[62px] aspect-square rounded-[10px] mt-[16px] cursor-pointer shadow-[0px_1px_8px_0px_#00000059]"
-        :class="selectedImage == i ? 'border-[2px] border-[#ffffff]' : ''"
-        @click="selectedImage = i"
-      />
-    </div>
+    <section class="w-full flex flex-col items-center px-4">
+      <!-- تصویر اصلی -->
+      <div class="relative w-full max-w-md aspect-square rounded-2xl overflow-hidden mt-5">
+        <img v-for="(image, i) in gallery" :key="i" :src="image" alt=""
+          class="absolute inset-0 w-full h-full object-cover rounded-2xl transition-opacity duration-300"
+          :class="i === selectedImage ? 'opacity-100 z-10' : 'opacity-0 z-0'" />
+      </div>
 
-    <div class="flex flex-col px-[16px]">
-      <h2 class="text-xl font-bold mt-[30px]">
+      <!-- تصاویر کوچک -->
+      <div class="flex items-center h-24 gap-4 w-full overflow-x-auto justify-center">
+        <img v-for="(image, i) in gallery" :key="i" :src="image" alt=""
+          class="transition-all duration-300 cursor-pointer rounded-[10px]" :class="selectedImage === i
+            ? 'w-[74px] h-[74px] border-x-[3px] border-x-red-500  z-10'
+            : 'w-[56px] h-[56px] scale-100 z-0'" @click="selectedImage = i" />
+      </div>
+
+    </section>
+
+
+    <section class="flex flex-col px-[16px]">
+      <h2 class="text-xl font-bold mt-[16px]">
         <span class="text-brand">کلینیک تــــن ساز</span> متن معرفی
       </h2>
 
-      <p class="mt-[12px] text-[14px]">
+      <p class="mt-[12px] text-[#2E2E2E] text-[14px] text-justify leading-[1.625rem]">
         لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده
         از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و
         سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای
@@ -79,70 +152,47 @@ onMounted(() => {
         پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
       </p>
 
-      <button
-        class="mt-[33px] w-full bg-brand text-center rounded-full h-[48px]"
-      >
-        <NuxtLink
-          to="#"
-          class="bg-brand text-white font-semibold leading-[26px] rounded-full w-full"
-          >تور مجازی کلینیک تن ساز</NuxtLink
-        >
+      <button class="mt-[33px] w-full bg-brand text-center rounded-full h-[48px]">
+        <NuxtLink to="#" class="bg-brand text-white font-semibold leading-[26px] rounded-full w-full">تور مجازی کلینیک
+          تن ساز</NuxtLink>
       </button>
-    </div>
+    </section>
 
-    <div
-      class="flex flex-col py-[60px] px-[16px] gap-[30px] w-full bg-[#1D1D1D] mt-[43px]"
-    >
+    <section class="flex flex-col py-[60px] px-[16px] gap-[30px] w-full bg-[#1D1D1D] mt-[43px]">
       <p class="text-[20px] font-bold text-[#ffffff]">
-        <span>مزیت های کلینیک </span>
+        <span>چرا کلینیک </span>
         <span class="text-brand">تــــــن ســـــــــــــاز </span>
         <span>؟</span>
       </p>
 
       <div class="flex flex-col gap-[25px]">
-        <div class="flex gap-[8px] items-start" v-for="x in 5">
-          <img src="/icons/red-hearth.svg" alt="" class="mt-[2px]" />
+        <div class="flex gap-[8px] items-start" v-for="item in whyTansaz" :key="item.id">
+          <img src="/icons/books.svg" alt="" class="mt-[2px]" />
 
           <div class="flex flex-col gap-[2px]">
-            <p class="text-[16px] font-semibold leading-[26px] text-[#ffffff]">
-              مواد با کیفیت
-            </p>
+            <p class="text-[16px] font-semibold leading-[26px] text-[#ffffff]">{{ item.title }}</p>
 
-            <p class="text-[12px] leading-[22px] text-[#ffffff]">
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-              استفاده با تولید سادگی از طراحان گرافیک است.
-            </p>
+            <p class="text-[12px] leading-[22px] text-[#ffffff]">{{ item.description }}</p>
           </div>
         </div>
       </div>
-    </div>
+    </section>
 
-    <div class="flex flex-col gap-[40px] w-full px-[16px] mb-[40px]">
+    <section class="flex flex-col gap-[40px] w-full px-[16px] mb-[40px]">
       <div class="flex flex-col mt-[43px] w-full">
         <p class="text-[20px] font-bold text-[#ffffff]">
-          <span class="text-brand">متخصصان </span>
-          <span class="text-[#000000]">ما</span>
+          <span class="text-brand">پزشکان </span>
+          <span class="text-[#000000]">مجموعه</span>
         </p>
         <p class="text-[14px] text-[#000000]">
           لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم است.
         </p>
       </div>
 
-      <div class="grid grid-cols-2 gap-x-[16px] gap-y-[30px]">
-        <div class="flex flex-col items-center" v-for="x in 6">
-          <img
-            src="/images/Surgery1.png"
-            alt=""
-            class="w-full rounded-[22px]"
-          />
-
-          <p class="mt-[16px] text-[16px] font-semibold text-[#2E2E2E]">
-            ساغر میرعباسی
-          </p>
-          <p class="text-[14px] font-medium text-brand">متخصص زیبایی</p>
-        </div>
+      <div class="flex flex-col gap-y-[1.25rem]">
+        <AboutUsDoctorCard v-for="doctor in doctors" :key="doctor.id" :doctor="doctor" />
       </div>
-    </div>
-    <HomeBlogsSection :is-transparent="true" />
+    </section>
+    <AboutUsEventsSection :is-transparent="true" />
   </div>
 </template>
